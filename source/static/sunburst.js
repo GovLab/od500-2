9ea7ca5602d73c993e720c80c0ec41b7b3a874cf
Denny Tek <tek.denny@gmail.com>
2016-08-19 17:54:32 -0400

    Add descriptors to data

--- source/static/sunburst.js
+++ source/static/sunburst.js
@@ -50,7 +50,7 @@ function format_description(d) {
   }
 
   if (d.usedBy_count) {
-    html += '<div class="tooltip-used-by">Used by '+ d.usedBy_count
+    html += '<div class="tooltip-used-by">'+ d.usedBy_count
     console.log(d.usedBy_count)
   }
   if (d.fte) {
@@ -137,7 +137,7 @@ d3.select(self.frameElement).style("height", height + "px");
 // function arcTween(d) {
 //   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
 //       yd = d3.interpolate(y.domain(), [d.y, 1]),
-//       yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+//       yr = d3.interpolate(y.range(), [d.y ? 0 : 0, radius]);
 //   return function(d, i) {
 //     return i
 //         ? function(t) { return arc(d); }
@@ -145,7 +145,7 @@ d3.select(self.frameElement).style("height", height + "px");
 //   };
 // }
 
-function computeTextRotation(d) {
-  return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
-}
+// function computeTextRotation(d) {
+//   return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
+// }
 })
