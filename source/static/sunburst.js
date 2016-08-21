411c150fd497789df174597452ca9ec7aa10a6e4
Denny Tek <tek.denny@gmail.com>
2016-08-21 18:35:36 -0400

    Fix transition

--- source/static/sunburst.js
+++ source/static/sunburst.js
@@ -68,7 +68,7 @@ d3.json("sunburst-no-data-new.json", function(error, root) {
 
   var path = g.append("path")
     .attr("d", arc)
-    .attr("class", function(d) { return "layer-" + d["depth"]} )
+    .attr("class", function(d) { return "layer-" + d.depth } )
     .on("click", click)
     .on("mouseover", function(d) {
       var sequenceArray = getAncestors(d);
@@ -165,7 +165,7 @@ function formatTrail(array) {
 function arcTween(d) {
   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
       yd = d3.interpolate(y.domain(), [d.y, 1]),
-      yr = d3.interpolate(y.range(), [d.y ? 0 : 0, radius]);
+      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
   return function(d, i) {
     return i
         ? function(t) { return arc(d); }
